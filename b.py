0750369ebe3204519025bb077f616bde6f7c8320
kamalkashyap13 <kamal.kashyap13@gmail.com>
2017-03-22 20:47:54 +0530

    Introduce how to take input

--- b.py
+++ b.py
@@ -1,4 +1,10 @@
-#b file 
-a = [1,2,3,4]
-print a
-# a is a python list
+def battleship():
+	return "bvhv"
+
+
+print "For battleship type 1"
+a = input()
+if a==1:
+	print "Battleship"
+else:
+	print "Re-print" 
